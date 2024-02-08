928ac721950ca05c4d6e97d1a53e7be8b61e81ce
Guga Villar <grsv.21@gmail.com>
2024-02-08 15:58:54 -0300

    :wrench: Update prettier config, add env types, add alias to path and install tailwind-merge

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_MAP_URL: string
+  readonly VITE_MAP_LAT: number
+  readonly VITE_MAP_LNG: number
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
