a9d6a3f874156b8d554807a8030e0fbff31150a2
Guga Villar <grsv.21@gmail.com>
2024-02-09 12:59:10 -0300

    :recycle: Update to new form with createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
 
-const root = document.getElementById('root')!
+const container = document.getElementById('root')!
+const root = createRoot(container)
 
-ReactDOM.createRoot(root).render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
