39019172fd812287c9527d50d78391728eb29f80
Guga Villar <grsv.21@gmail.com>
2024-02-09 08:44:26 -0300

    :sparkles: Create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className="flex w-full flex-col items-center justify-center gap-4 border-b-2 border-b-slate-500 bg-gray-50 p-5">
+      <h1 className="text-center text-xl font-semibold">
+        Explore a evolução populacional
+        <em className="block text-lg">São José dos Campos</em>
+      </h1>
+      <h2 className="text-center">clique nos bairros destacados</h2>
+    </header>
+  )
+}
